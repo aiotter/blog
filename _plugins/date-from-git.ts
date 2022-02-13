3ceb3d9dbe016df965d898010e3dd2877a996c5c
aiotter <git@aiotter.com>
2022-02-13 16:06:18 +0900

    fix(plugin): Wrong last modified date fetched from git

--- _plugins/date-from-git.ts
+++ _plugins/date-from-git.ts
@@ -4,19 +4,45 @@ import { Page, Site } from "lume/core.ts";
 
 async function getLastModified(site: Site, page: Page) {
   try {
-    const [log] = await git.log({
+    const dir = site.src();
+
+    const filepath = (page.src.path.startsWith("/")
+      ? page.src.path.slice(1)
+      : page.src.path) + (page.src.ext ?? "");
+
+    const commits = (await git.log({
       fs,
-      dir: site.src(),
-      filepath: (page.src.path.startsWith("/")
-        ? page.src.path.slice(1)
-        : page.src.path) + (page.src.ext ?? ""),
-      depth: 1,
+      dir,
+      filepath,
+      follow: true,
+    }))
+      // Patch a bug on isomorphic-git:
+      // prevent accidentally listing up the root branch from merged branch
+      .filter((commit, index, array) =>
+        !(commit.commit.parent.length === 0 && array.length - 1 > index)
+      );
+
+    if (commits.length == 1) return commits[0];
+
+    const targetBlob = await git.readBlob({
+      fs,
+      dir,
+      oid: await git.resolveRef({ fs, dir, ref: "HEAD" }),
+      filepath,
     });
-    return log;
-  } catch (error) {
-    if (error instanceof git.Errors.NotFoundError) {
-      return undefined;
+
+    for (const [i, commit] of commits.entries()) {
+      const containedOids: string[] = await git.walk({
+        fs,
+        dir: Deno.cwd(),
+        trees: [git.TREE({ ref: commit.oid })],
+        map: async (_filename, [entry]) => await entry!.oid(),
+      });
+
+      if (!containedOids.includes(targetBlob.oid)) return commits[i - 1];
     }
+  } catch (error) {
+    if (error instanceof git.Errors.NotFoundError) return undefined;
     throw error;
   }
 }
@@ -25,7 +51,7 @@ async function setLastModified(site: Site, page: Page) {
   const log = await getLastModified(site, page);
 
   if (log) {
-    page.data.lastModified ||= new Date(log.commit.author.timestamp);
+    page.data.lastModified ||= new Date(log.commit.author.timestamp * 1000);
     page.data.commit ||= log.oid;
   }
 }
