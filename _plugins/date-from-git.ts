ed1796a3f0c6507d5700bd96dc853582e9b75ade
aiotter <git@aiotter.com>
2022-02-17 22:32:54 +0900

    fix(plugin): Wrong date detection from git

--- _plugins/date-from-git.ts
+++ _plugins/date-from-git.ts
@@ -2,34 +2,32 @@ import * as fs from "https://deno.land/std@0.120.0/node/fs.ts";
 import * as git from "https://esm.sh/isomorphic-git@1.10.3";
 import { Page, Site } from "lume/core.ts";
 
-async function getLastModified(site: Site, page: Page) {
-  try {
-    const dir = site.src();
+async function getCommits(site: Site, page: Page) {
+  const dir = site.src();
 
-    const filepath = (page.src.path.startsWith("/")
-      ? page.src.path.slice(1)
-      : page.src.path) + (page.src.ext ?? "");
+  const filepath =
+    (page.src.path.startsWith("/") ? page.src.path.slice(1) : page.src.path) +
+    (page.src.ext ?? "");
 
-    const commits = (await git.log({
-      fs,
-      dir,
-      filepath,
-      follow: true,
-    }))
+  const commits = await git.log({ fs, dir, filepath, follow: true })
+    .then((commits) =>
       // Patch a bug on isomorphic-git:
-      // prevent accidentally listing up the root branch from merged branch
-      .filter((commit, index, array) =>
+      // prevent accidentally listing up the root branch of merged branch
+      commits.filter((commit, index, array) =>
         !(commit.commit.parent.length === 0 && array.length - 1 > index)
-      );
-
-    if (commits.length == 1) return commits[0];
-
-    const targetBlob = await git.readBlob({
-      fs,
-      dir,
-      oid: await git.resolveRef({ fs, dir, ref: "HEAD" }),
-      filepath,
+      )
+    )
+    .catch((error) => {
+      if (error instanceof git.Errors.NotFoundError) return [];
+      throw error;
     });
+
+  const created = commits[0] as git.ReadCommitResult | undefined;
+  if (commits.length == 1) return { created, lastModified: undefined };
+
+  try {
+    const headOid = await git.resolveRef({ fs, dir, ref: "HEAD" });
+    const targetBlob = await git.readBlob({ fs, dir, oid: headOid, filepath });
 
     for (const [i, commit] of commits.entries()) {
       const containedOids: string[] = await git.walk({
@@ -39,46 +37,33 @@ async function getLastModified(site: Site, page: Page) {
         map: async (_filename, [entry]) => await entry!.oid(),
       });
 
-      if (!containedOids.includes(targetBlob.oid)) return commits[i - 1];
+      if (!containedOids.includes(targetBlob.oid)) {
+        return { created, lastModified: commits[i - 1] };
+      }
     }
+    return { created, lastModified: undefined };
   } catch (error) {
-    if (error instanceof git.Errors.NotFoundError) return undefined;
+    if (error instanceof git.Errors.NotFoundError) {
+      return { created, lastModified: undefined };
+    }
     throw error;
   }
 }
 
-async function getDate(site: Site, page: Page) {
-  try {
-    const dir = site.src();
-    const filepath = (page.src.path.startsWith("/")
-      ? page.src.path.slice(1)
-      : page.src.path) + (page.src.ext ?? "");
-
-    const commits = await git.log({ fs, dir, filepath, follow: true });
-    return commits[-1];
-  } catch (error) {
-    if (error instanceof git.Errors.NotFoundError) return undefined;
-    throw error;
+async function setDates(site: Site, page: Page) {
+  const { created, lastModified } = await getCommits(site, page);
+  if (created) {
+    page.data.date = new Date(created.commit.author.timestamp * 1000);
   }
-}
-
-async function setLastModified(site: Site, page: Page) {
-  const log = await getLastModified(site, page);
-
-  if (log) {
-    page.data.lastModified ||= new Date(log.commit.author.timestamp * 1000);
-    page.data.commit ||= log.oid;
+  if (lastModified) {
+    page.data.lastModified ||= new Date(
+      lastModified.commit.author.timestamp * 1000,
+    );
   }
-}
-
-async function setDate(site: Site, page: Page) {
-  const log = await getDate(site, page);
-  if (log) page.data.date = new Date(log.commit.author.timestamp * 1000);
 }
 
 export default function () {
   return (site: Site) => {
-    site.preprocess([".html"], setLastModified.bind(null, site));
-    site.preprocess([".html"], setDate.bind(null, site));
+    site.preprocess([".html"], setDates.bind(null, site));
   };
 }
