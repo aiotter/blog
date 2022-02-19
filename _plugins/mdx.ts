c0245cc5bbba74c3208432a013edf4d3ef2b95a0
aiotter <git@aiotter.com>
2022-02-19 11:09:39 +0900

    fix(plugin): LSP emit errors for every line on TSX files

--- _plugins/mdx.ts
+++ _plugins/mdx.ts
@@ -1,14 +1,12 @@
 import { evaluate } from "https://esm.sh/@mdx-js/mdx@v2.0.0?pin=v66&alias=astring:https://deno.land/x/astring@v1.8.1/src/astring.js";
 import type { Data, Engine, Site } from "lume/core.ts";
 import textLoader from "lume/core/loaders/text.ts";
-import { JsxEngine } from "lume/plugins/jsx.ts";
-import * as reactRuntime from "https://esm.sh/react@17.0.2/jsx-runtime.js";
 
 type EvaluateOptions = Parameters<typeof evaluate>[1];
 
 export interface Options {
   /** The list of extensions this plugin applies to */
-  extensions: string[] | {
+  extensions?: string[] | {
     pages: string[];
     components: string[];
   };
@@ -19,8 +17,6 @@ export interface Options {
 // Default options
 export const defaults: Partial<Options> = {
   extensions: [".mdx"],
-  mdxOptions: { ...reactRuntime, useDynamicImport: true } as EvaluateOptions,
-  jsxEngine: new JsxEngine(),
 };
 
 export async function mdxLoader(
@@ -35,8 +31,9 @@ export async function mdxLoader(
 }
 
 /** Register the plugin to support MDX files */
-export default function (userOptions?: Partial<Options>) {
-  const options = { ...defaults, ...userOptions } as Options;
+export default function (userOptions: Options) {
+  const options = { ...defaults, ...userOptions } as Required<Options>;
+
   const extensions = Array.isArray(options.extensions)
     ? { pages: options.extensions, components: options.extensions }
     : options.extensions;
